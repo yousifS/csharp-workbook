dcb357b1ba7cc007b3a5cb54d5d833965969c1bd
Yousif Seedhom <yousif.seedhom@gmail.com>
2019-04-04 18:50:32 -0500

    Updated the hello world program

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // print out a greeting to the screen
+            Console.WriteLine("Hello my name is Bob.");
+            // ask the user for their name
+            Console.WriteLine("What is your name?");
+            // read the name they type in into a String variable
+            String userInput = Console.ReadLine();
+            // print a customized message back
+            Console.WriteLine("Nice to meet you "+userInput);
+
         }
     }
 }
