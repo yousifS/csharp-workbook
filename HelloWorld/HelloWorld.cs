08bb75a6d6d5f59da2e04a23c6433ac35753b80f
Yousif Seedhom <yousif.seedhom@gmail.com>
2019-04-04 18:46:08 -0500

    Initial Hello world

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,17 +6,35 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello fellow traveler");
-            Console.WriteLine("My name is yousif.");
-            Console.WriteLine("What is your name? ");
-            String yourName = Console.ReadLine();
             
-            if(yourName == "Bob"){
-                Console.WriteLine("Really? Bob is the best you could come up with");
-            } else {
-                Console.WriteLine("Hello "+yourName);
-            }
-            
+            // define a variable as num1
+            int num1;
+            // define a variable as num2
+            int num2;
+
+            // define a string variable to hold the user input as a string
+            String userInput;
+            // ask the user for the first number
+            Console.WriteLine("Enter the first number.");
+            // read the user's input as a string
+            userInput = Console.ReadLine();
+            // convert the string user input to an int
+            num1 = Convert.ToInt32(userInput);
+
+            // ask the user for the second number
+            Console.WriteLine("Enter the second number.");
+            // read the user's input as a string
+            userInput = Console.ReadLine();
+            // convert the string user input to an int and store it as num2
+            num2 = Convert.ToInt32(userInput);
+
+            // define a variable called sum the holds the sum of num1 and num2
+            int sum = num1 + num2;
+            // print out a message to the screen that shows the 
+            // 2 numbers and their sum.
+            Console.WriteLine("The sum of "+num1+" and "+num2+" is "+sum);
+
+
         }
     }
 }
